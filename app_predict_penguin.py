52c4344bb38e91979939b0dde8f6c1c9f53528f1
ptungjitsirisun <129649797+ptungjitsirisun@users.noreply.github.com>
2023-04-09 14:03:51 +0700

    Update app_predict_penguin.py

--- app_predict_penguin.py
+++ app_predict_penguin.py
@@ -1,7 +1,7 @@
 
 import streamlit as st 
 from streamlit_option_menu import option_menu
-import plotly.graph_objects as px
+#import plotly.graph_objects as px
 import numpy as np
 import pandas as pd
 import pickle
